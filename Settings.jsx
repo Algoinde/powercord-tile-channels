0dd6d44266cc4bc59d20bdd7c3c02fe258da9501
Algoinde <algoinde@gmail.com>
2021-03-17 14:34:17 +0300

    Added settings, made everything make more sense

---
+++ Settings.jsx
@@ -0,0 +1,35 @@
+const { React } = require('powercord/webpack');
+const { getModule } = require('powercord/webpack');
+const { SwitchItem } = require('powercord/components/settings');
+
+module.exports = class Settings extends React.Component {
+	constructor (props) {
+		super(props);
+
+		const get = props.getSetting;
+		this.state = {
+			guildMode: get('guildMode', {})
+		};
+	}
+
+	async componentDidMount () {
+		// this.setState({
+		//   guildMode: ''
+		// });
+	}
+
+	render () {
+		const { getSetting, toggleSetting, _render } = this.props;
+		return (
+			<>
+				<SwitchItem note='To enable Tile Channels per-server, use the server menu dropdown.'
+					value={getSetting('guildMode', false)}
+					onChange={() => {
+						toggleSetting('guildMode');
+						_render(true);
+					}}
+				>Disable TileChannels by default</SwitchItem>
+			</>
+		);
+	}
+};
--- index.js
+++ index.js
@@ -11,264 +11,159 @@ const {
 	inject,
 	uninject
 } = require('powercord/injector');
+const { forceUpdateElement, findInReactTree } = require('powercord/util')
 const {
 	Tooltip
 } = require('powercord/components');
 
-// const Settings = require('./Settings');
+const Settings = require('./Settings');
+const getGuild = getModule(["getGuildId","getLastSelectedGuildId"], false);
 
 module.exports = class TileChannels extends Plugin {
-		constructor() {
-			super();
+	constructor() {
+		super();
+	}
+
+	async startPlugin() {
+	    powercord.api.settings.registerSettings('powercord-tile-channels', {
+			category: this.entityID,
+			label: 'TileChannels',
+			render: (props) => React.createElement(Settings, {
+				_render: this._render.bind(this),
+				...props
+			})
+		});
+		this.loadStylesheet('./main.css');
+		if(this.settings.get('servers') == undefined) {
+			this.settings.set('servers', []);
 		}
 
-		async startPlugin() {
-			// powercord.api.settings.registerSettings('alg-channels', {
-			//   category: this.entityID,
-			//   label: 'TileChannels',
-			//   render: Settings
-			// });
-			var style = document.createElement('style');
-			style.innerText = `
-#channels .containerDefault--pIXnN {
-	width: 25%;
-}
-
-#channels .containerDefault-3tr_sE {
-	width: 100%;
-}
-
-#channels .content-3YMskv {
-	display: flex;
-	justify-content: flex-start;
-	flex-wrap: wrap;
-	align-content: start;
-	align-items: flex-start;
-}
-#channels .content-3YMskv > div:first-child {
-	width: 100%;
-}
+		const ScrollObject = await getModule(m => m && m.default && m.default.prototype && m.default.prototype.getHeightForFooter);
+		const NavigableChannels = await getModule(m => m.default && m.default.displayName == 'NavigableChannels');
+		const ChannelItem = await getModule(m => m.default ?.displayName === 'ChannelItem');
+		const Menu = await getModule(m => m.default ?.displayName === 'Menu');
+		const MenuCheckboxItem = await getModule(['MenuCheckboxItem']);
 
 
-#channels .children-3rEycc .iconBase-3LOlfs {
-	display: none;
-}
+		inject('tile-channels-update', NavigableChannels, 'default', (_, res) => {
+			if(this.currentServer != getGuild.getGuildId()) {
+				this.currentServer = getGuild.getGuildId();
+				this._render();
+			}
 
-#channels .iconContainer-1BBaeJ {
-	display: none;
-}
+			return _;
+		}, true);
+		NavigableChannels.default.displayName = 'NavigableChannels';
 
-#channels a.mainContent-u_9PKf[aria-label*="mention"]:before {
-	display: block;
-	border-radius: 4px;
-	box-shadow: 0 0 10px 8px rgba(255,0,0,0.2) inset;
-	pointer-events: none;
-}
+		const self = this;
+		inject('tile-channels-rowHeight', ScrollObject.default.prototype, 'getHeightForRow', function(_, res) {
+			if(!self.serverMatch) return res;
+			if (typeof this.rowHeight == 'function')
+				return Math.round(res / 4);
+			else
+				return res;
+		});
 
-#channels .name-23GUGE.overflow-WK9Ogt {
-	text-overflow: clip;
-	font-size: 0.9em;
-}
-#channels .mainContent-u_9PKf .name-23GUGE.overflow-WK9Ogt:after {
-	position: absolute;
-	visibility: hidden;
-	pointer-events: none;
-}
+		inject('tile-channels-server-menu', Menu, 'default', (function(args, ret) {
+			if(args[0].navId != 'guild-header-popout') return args;
+			// console.log(args,ret,ChannelItem);
+			if(!findInReactTree(args[0],m => m.id == 'enable-tile-channels')) {
+				this.checkbox = React.createElement(MenuCheckboxItem.MenuCheckboxItem, {
+					id: "enable-tile-channels",
+					key: "enable-tile-channels",
+					label: "Enable Tile Channels",
+					name: "Enable Tile Channels",
+					color: "colorBrand",
+					checked: this._queryServer(),
+					action: () => {
+						this.checkbox.props.checked = this._toggleServer();
+						// this.checkbox.forceUpdate();
+					},
+				});
+			let children = findInReactTree(args[0], m => m[0] && Array.prototype.some.call(m, ch => ch && ch.props && ch.props.id == 'hide-muted-channels'))
+				if(children) children.push(this.checkbox);
+			}
+			return args;
+		}).bind(this),true)
+		Menu.default.displayName = 'Menu';
 
-#channels .iconVisibility-sTNpHs.wrapper-2jXpOf.modeUnread-1qO3K1 .unread-2lAfLh {
-	display: none;
-}
 
-#channels .iconVisibility-sTNpHs.wrapper-2jXpOf.modeUnread-1qO3K1 .content-1x5b-n {
-	/* background: rgba(0,0,0,0.08); */
-	box-shadow: 0 0 9px 9px #2f3136ff inset, 0 0 0 50px rgba(255,255,255,0.04) inset;
-	/* border-radius: 6px; */
-}
-#channels .iconVisibility-sTNpHs.wrapper-2jXpOf.modeUnread-1qO3K1 .content-1x5b-n:hover {
-		box-shadow: none;
-}
-
-#channels .content-1x5b-n {
-	text-align: center;
-	padding: 0 2px;
-}
-#channels .list-2luk8a.list-SuzGBZ.listDefault-3ir5aS {
-	padding-left: 0;
-}
-
-#channels .avatarContainer-28iYmV.avatar-3tNQiO.avatarSmall-1PJoGO {
-	z-index: -1;
-	left: 50%;
-	position: absolute;
-	top: 2px;
-	opacity: 0.3;
-	transform: translateX(-89%);
-}
-.liveIconSpacing-DSnkAT {
-	opacity: 0.8;
-}
-.modeUnread-1qO3K1 a.mainContent-u_9PKf[aria-label*="elysium"],
-.modeUnread-1qO3K1 a.mainContent-u_9PKf[aria-label*="discipl"],
-.modeUnread-1qO3K1 a.mainContent-u_9PKf[aria-label*="committ"],
-.modeUnread-1qO3K1 a.mainContent-u_9PKf[aria-label*="popo-ch"] {
-	box-shadow: 0px 0px 6px 4px rgba(78, 189, 255, 0.21) inset;
-	border-radius: 4px;
-}
-.mainContent-u_9PKf[aria-label^="🔹"],
-.mainContent-u_9PKf[aria-label^="🔸"],
-.mainContent-u_9PKf[aria-label^="unread, 🔹"],
-.mainContent-u_9PKf[aria-label^="unread, 🔸"],
-.reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-{
-	text-indent: -4px;
-}
-
-.emoji-negative {
-	text-indent: -4px;
-}
-.channel-typing {
-	position: absolute;
-	bottom: -1px;
-	left: 50%;
-	height:12px;
-	transform: translateX(-52%) scale(0.9);
-	overflow: hidden;
-	pointer-events: none;
-}
-.list-2luk8a.list-SuzGBZ.listDefault-3ir5aS {
-	margin-right: -8px;
-}
-.content-1Wq3SX {
-	height: 32px;
-	padding: 0 5px;
-}
-.icons-1dXQdz {margin-right: 0;margin-left: 6px;}
-.children-3rEycc .ucbadge {
-	background-color: #7289da33 !important;
-    position: absolute;
-    margin: 0;
-    line-height: 1;
-    pointer-events: none;
-    top: -4px;
-    right: -4px;
-}
-`;
-
-			this.style = document.body.appendChild(style);
-
-			const channels = await getModule(function(e) {
-				if (e && e.default && e.default.prototype && e.default.prototype.getHeightForFooter) return true;
-			})
-			const NavigableChannels = await getModule(m => m.default && m.default.displayName == 'NavigableChannels');
-
-			inject('alg-channels-rowHeight', channels.default.prototype, 'getHeightForRow', function(_, res) {
-				if (typeof this.rowHeight == 'function')
-					return Math.round(res / 4);
-				else
-					return res;
-			});
-			this.channelCache = {};
-			this.deep = undefined;
-
-			const ChannelItem = await getModule(m => m.default ?.displayName === 'ChannelItem');
-
-			//Stores the channel name, fakes it 
-			inject('alg-cha', ChannelItem, 'default', (args, res) => {
-				this.channelCache[args[0].channel.id] = args[0].channel.name;
-				try{
-					name = args[0].channel.name;
-					if (!name) return args;
-					var split = name.split('-').filter(sp => sp != '').map(s => Array.from(s));
-					var s = '';
-					switch (true) {
-						case split.length == 4:
-							s = ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][0] : split[0][1]) + split[1][0] + split[2][0] + split[3][0];
-							break;
-						case split.length == 3:
-							s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? '' : split[0][1] || '') + split[1][0] + split[2][0];
-							break;
-						case split.length == 2:
-							s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][1] + '-' : split[0][1]) + split[1][0] + split[1][1];
-							break;
-						default:
-							if (split[0] && split[0].length < 5)
-								s = split[0].join('');
-							else
-								s = (split[0] || []).filter((ch, index) => !(/[aeiou]/.test(ch) && index > 0)).slice(0, 4).join('') || '';
-							break;
-					}
-					args[0].channel.name = s;
-				} catch (e) {
-					console.error('[TileChannels]:', e)
+		inject('tile-channels-rename', ChannelItem, 'default', (args, res) => {
+			if(!this.serverMatch) return res;
+		let name = res.props.children.props.children[1].props.children[0].props.children[1].props.children[0];
+			try{
+				if (!name) return res;
+				var split = name.split('-').filter(sp => sp != '').map(s => Array.from(s));
+				var s = '';
+				switch (true) {
+					case split.length == 4:
+						s = ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][0] : split[0][1]) + split[1][0] + split[2][0] + split[3][0];
+						break;
+					case split.length == 3:
+						s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? '' : split[0][1] || '') + split[1][0] + split[2][0];
+						break;
+					case split.length == 2:
+						s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][1] + '-' : split[0][1]) + split[1][0] + split[1][1];
+						break;
+					default:
+						if (split[0] && split[0].length < 5)
+							s = split[0].join('');
+						else
+							s = (split[0] || []).filter((ch, index) => !(/[aeiou]/.test(ch) && index > 0)).slice(0, 4).join('') || '';
+						break;
 				}
-				return args;
-			}, true);
-			ChannelItem.default.displayName = 'ChannelItem';
-
-			//And then puts it back when the channel render is finished. DIRTY.
-			inject('alg-cha-post', ChannelItem, 'default', (args, res) => {
-				args[0].channel.name = this.channelCache[args[0].channel.id];
-				const Popout = React.createElement(Tooltip, {
-					text: args[0].channel.name,
-					position: 'top',
-					color: "primary",
-					delay: 200,
-					disableTooltipPointerEvents: true,
-				}, res)
-				return Popout;
-			});
-
-			ChannelItem.default.displayName = 'ChannelItem';
-
-
-			//Old solution with querySelector, has delay.
-			// 	inject('alg-channels-compute', NavigableChannels, 'default', (_, res) => {
-			// 		if (!document.getElementById('channels')) return res;
-			// 		setTimeout(() => {
-			// 			if (!document.getElementById('channels')) return;
-			// 			Array.prototype.forEach.call(document.getElementById('channels').querySelectorAll(`.name-23GUGE.overflow-WK9Ogt`), item => {
-			// 				if (!item) return res;
-			// 				if (item.edited) return res;
-			// 				item.edited = true;
-			// 				item.setAttribute('data-original', item.innerText);
-			// 				try {
-			// 					var split = item.innerText.split('-').filter(sp => sp != '').map(s => Array.from(s));
-			// 					if (split[0] && split[0][0] && split[0][0].length > 1) item.classList.add('emoji-negative');
-			// 					else item.classList.remove('emoji-negative');
-			// 					var s = '';
-			// 					switch (true) {
-			// 						case split.length == 4:
-			// 							s = ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][0] : split[0][1]) + split[1][0] + split[2][0] + split[3][0];
-			// 							break;
-			// 						case split.length == 3:
-			// 							s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? '' : split[0][1] || '') + split[1][0] + split[2][0];
-			// 							break;
-			// 						case split.length == 2:
-			// 							s = split[0][0] + ((/^[\x00-\x7F]*$/.test(split[0][0])) ? split[0][1] + '-' : split[0][1]) + split[1][0] + split[1][1];
-			// 							break;
-			// 						default:
-			// 							if (split[0] && split[0].length < 5)
-			// 								s = split[0].join('');
-			// 							else
-			// 								s = (split[0] || []).filter((ch, index) => !(/[aeiou]/.test(ch) && index > 0)).slice(0, 4).join('') || '';
-			// 							break;
-			// 					}
-			// 					item.innerText = s;
-			// 				} catch (e) {
-			// 					console.error('[TileChannels]:', e)
-			// 				}
-			// 			})
-			// 		}, 5);
-			// 		return res;
-			// 	})
-			// 	NavigableChannels.default.displayName = 'NavigableChannels';
+				res.props.children.props.children[1].props.children[0].props.children[1].props.children[0] = s;
+			} catch (e) {
+				console.error('[TileChannels]:', e)
 			}
 
-			pluginWillUnload() {
-				// uninject('alg-channels-compute');
-				uninject('alg-channels-rowHeight');
-				uninject('alg-cha');
-				uninject('alg-cha-post');
-				document.body.removeChild(this.style);
-			}
-		};
+			const Popout = React.createElement(Tooltip, {
+				text: args[0].channel.name,
+				position: 'top',
+				color: "primary",
+				delay: 200,
+				disableTooltipPointerEvents: true,
+			}, res)
+			return Popout;
+		});
+		ChannelItem.default.displayName = 'ChannelItem';
+	}
+
+	_render(redraw) {
+		if(!document.querySelector('.sidebar-2K8pFh')) return;
+		this.serverMatch = this._queryServer();
+		if(this.serverMatch) {
+			document.querySelector('.sidebar-2K8pFh').classList.add('tiles');
+		}else{
+			document.querySelector('.sidebar-2K8pFh').classList.remove('tiles');
+		}
+		if(redraw)forceUpdateElement('.containerDefault--pIXnN', true);
+	}
+
+	_toggleServer(state) {
+	let id = getGuild.getGuildId();
+	let serverArray = this.settings.get('servers');
+	let idx = serverArray.indexOf(id);
+		if(idx > -1) {
+			serverArray.splice(idx, 1);
+		}else{
+			serverArray.push(id)
+		}
+		this.settings.set('servers', serverArray);
+		this._render(true);
+		return this._queryServer();
+	}
+
+	_queryServer() {
+	let id = getGuild.getGuildId()
+		return this.settings.get('servers').includes(id) ^ this.settings.get('guildMode');
+	}
+
+	pluginWillUnload() {
+		powercord.api.settings.unregisterSettings('powercord-tile-channels');
+		uninject('tile-channels-server-menu');
+		uninject('tile-channels-rowHeight');
+		uninject('tile-channels-rename');
+		uninject('tile-channels-update');
+	}
+};
